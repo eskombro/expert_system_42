fb9b693e192f1e83a658d6024babe5f1ef71e506
Samuel Jimenez <sjimenezre@gmail.com>
2019-10-17 21:12:11 +0200

    Parsing + error handling AND main loop structure

--- main.go
+++ main.go
@@ -1,89 +1,50 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"regexp"
-	"strings"
+	// "regexp"
+	p "expert_system_42/parser"
 )
 
-const (
-	FALSE = iota
-	TRUE
-	UNDEFINED
-)
-
-type Input struct {
-	initialFacts string
-	queries      string
-	rules        []string
-	elements     map[string]int
-}
-
-func check(e error) {
-	if e != nil {
-		fmt.Println("Error:", e.Error())
-		os.Exit(1)
+func initializeElementsMap(input *p.Input, step int) {
+	for _, c := range input.InitialFacts[step] {
+		str := fmt.Sprintf("%c", c)
+		input.Elements[str] = p.FALSE
 	}
 }
 
-func handleInput() Input {
-	f, err := os.Open(os.Args[1])
-	defer f.Close()
-	check(err)
-	input := Input{}
-	input.elements = make(map[string]int)
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line := scanner.Text()
-		// Delete comments
-		commentStart := strings.Index(line, "#")
-		if commentStart != -1 {
-			line = line[:commentStart]
-		}
-		// Whitespaces and empty line handle
-		space := regexp.MustCompile(`\s+`)
-		line = space.ReplaceAllString(line, "")
-		if len(line) == 0 {
-			continue
-		}
-		// Fill imput structure
-		if strings.Index(line, "=") == 0 {
-			input.initialFacts = line[1:]
-		} else if strings.Index(line, "?") == 0 {
-			input.queries = line[1:]
-		} else {
-			input.rules = append(input.rules, line)
-		}
-		// Create elements in map
-		specialChars := "?=<> +!^|()"
-		for _, c := range line {
-			str := fmt.Sprintf("%c", c)
-			if strings.Index(specialChars, fmt.Sprint(str)) == -1 {
-				input.elements[str] = FALSE
-			}
-		}
+func applyRules(input *p.Input, step int) {
+	for _, rule := range input.Rules{
+		fmt.Println("Rule:", rule)
 	}
-	return input
 }
+
 
 func main() {
 	fmt.Println("Launching expert system...")
 	if len(os.Args) != 2 {
 		fmt.Println("Error in args. Bye :)")
+		os.Exit(1)
 	}
-	input := handleInput()
-	// Handle Inital facts
-	for _, fact := range input.initialFacts {
-		f := fmt.Sprintf("%c", fact)
-		// fmt.Println(f)
-		input.elements[f] = TRUE
-	}
-	fmt.Println(input)
-	// Print result
-	for _, query := range input.queries {
-		f := fmt.Sprintf("%c", query)
-		fmt.Println(f, input.elements[f])
+	input := p.ParseInput()
+	for step := range input.Queries{
+
+		// Initialize map with Rules
+		initializeElementsMap(&input, step)
+
+		// Handle Inital facts
+		fmt.Println("Facts:", input.InitialFacts[step])
+		for _, fact := range input.InitialFacts[step] {
+			f := fmt.Sprintf("%c", fact)
+			input.Elements[f] = p.TRUE
+		}
+
+		// Handle Rules
+		applyRules(&input, step)
+
+		// Handle query
+		fmt.Println("Query:", input.Queries[step])
+
 	}
 }
---
+++ parser/parser.go
@@ -0,0 +1,140 @@
+package parser
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+const (
+	FALSE = iota
+	TRUE
+	UNDEFINED
+)
+
+type Input struct {
+	InitialFacts []string
+	Queries      []string
+	Rules        []string
+	Elements     map[string]int
+}
+
+func checkError(e error) {
+	if e != nil {
+		fmt.Println("Error:", e.Error())
+		os.Exit(1)
+	}
+}
+
+func throwParsingLineError(e string, line string) {
+	fmt.Println("Error:", e)
+	fmt.Println("     Line:", line)
+	os.Exit(1)
+}
+
+func throwParsingError(e string) {
+	fmt.Println("Error:", e)
+	os.Exit(1)
+}
+
+func cleanLine(line string) string {
+	// Delete comments
+	commentStart := strings.Index(line, "#")
+	if commentStart != -1 {
+		line = line[:commentStart]
+	}
+	// Whitespaces and empty line handle
+	space := regexp.MustCompile(`\s+`)
+	line = space.ReplaceAllString(line, "")
+	return line
+}
+
+func checkRuleSyntax(line *string) {
+	// Check both sides
+	if strings.Count(*line, "=>") != 1 {
+		throwParsingLineError("Rule is not well formated", *line)
+	}
+	checkLine := strings.Index(*line, "=>")
+	if checkLine == 0 || checkLine == len(*line)-2 {
+		throwParsingLineError("Rule is not well formated at both sides", *line)
+	}
+	// Check no other characters
+	hasValidCharacters := regexp.MustCompile(`^[A-Z+=><!^|()]+$`).MatchString
+	if !hasValidCharacters(*line) {
+		throwParsingLineError("Line has invalid characters", *line)
+	}
+}
+
+func checkValidInput(input *Input) {
+	// Check every element is present
+	if len(input.Rules) == 0 {
+		throwParsingError("Missing rules in file")
+	}
+	if len(input.InitialFacts) == 0 || len(input.Queries) == 0 {
+		throwParsingError("Missing facts or queries in file")
+	}
+	if len(input.InitialFacts) != len(input.Queries) {
+		throwParsingError("Not coherent count of Initial facts and Queries")
+	}
+}
+
+func initializeElementsMap(input *Input, line *string) {
+	// Create elements in map
+	for _, c := range *line {
+		str := fmt.Sprintf("%c", c)
+		isAlpha := regexp.MustCompile(`^[A-Z]`).MatchString
+		if isAlpha(fmt.Sprint(str)) {
+			input.Elements[str] = FALSE
+		}
+	}
+}
+
+func ParseInput() Input {
+
+	f, err := os.Open(os.Args[1])
+	checkError(err)
+	defer f.Close()
+
+	input := Input{}
+	input.Elements = make(map[string]int)
+	scanner := bufio.NewScanner(f)
+	readingRules := true
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = cleanLine(line)
+		if len(line) == 0 {
+			continue
+		}
+		// Check rule then facts and Queries
+		isFact := strings.Count(line, "=") == 1 && strings.Count(line, "=>") == 0
+		isQuery := strings.Count(line, "?") == 1
+		if readingRules {
+			if isFact || isQuery {
+				readingRules = false
+			}
+		} else {
+			if !isFact && !isQuery {
+				throwParsingLineError("Syntax error after rules", line)
+			}
+		}
+		// Append rule / facts / query
+		if readingRules {
+			checkRuleSyntax(&line)
+			input.Rules = append(input.Rules, line)
+		} else {
+			if isFact {
+				input.InitialFacts = append(input.InitialFacts, line[1:])
+			} else if isQuery {
+				input.Queries = append(input.Queries, line[1:])
+			} else {
+				throwParsingLineError("Syntax error after rules", line)
+			}
+		}
+		initializeElementsMap(&input, &line)
+	}
+	checkValidInput(&input)
+	return input
+}
